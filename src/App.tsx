96ef26fa2f69ea4abcec4f3951241156dbb43188
nowy <nowy@tomeks-iMac.T-Mobile>
2024-07-11 09:13:22 +0200

    Change map to fullscreen and Add auto-resize hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css'
-
-import Map from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
+import Map from 'react-map-gl/maplibre';
+import useWindowDimensions from './Hooks/useWindowDimensions';
+import { WindowDimensions } from './types/ui';
+
 function App() {
+  const { height, width } : WindowDimensions = useWindowDimensions();
+
   return (
     <>
       <Map initialViewState={{
         longitude: -122.4,
         latitude: 37.8,
         zoom: 14
-      }} style={{width:600, height: 400}} mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      }} style={{width: width, height: height}} mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
       />
     </>
   )
