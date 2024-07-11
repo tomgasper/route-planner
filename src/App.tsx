9b2dd3b6f927b34fd7ac2e14c39572e2a5756e2a
nowy <nowy@tomeks-iMac.T-Mobile>
2024-07-11 09:53:51 +0200

    Add Controlled Map state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import './App.css'
 import 'maplibre-gl/dist/maplibre-gl.css';
 
+import { useState } from 'react';
+
 import Map from 'react-map-gl/maplibre';
 import useWindowDimensions from './Hooks/useWindowDimensions';
 import { WindowDimensions } from './types/ui';
 
 function App() {
   const { height, width } : WindowDimensions = useWindowDimensions();
+  const [viewState, setViewState] = useState({
+    longitude: -122.4,
+    latitude: 37.8,
+    zoom: 14
+  });
 
   return (
     <>
-      <Map initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }} style={{width: width, height: height}} mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      <Map
+       {...viewState}
+       onMove={evt => setViewState(evt.viewState)}
+       style={{width: width, height: height}}
+       mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
       />
     </>
   )
