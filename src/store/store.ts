ea0597ee3b402e4e7f35bdc4eef576b0b9ed1a33
nowy <nowy@tomeks-iMac.T-Mobile>
2024-07-12 12:08:54 +0200

    Add persistent storage for Map Points

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mapPointsReducer from "./mapPointsSlice";
+import { persistReducer, persistStore , persistCombineReducers} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
 
-export const store = configureStore({
-    reducer: {
-        mapPoints: mapPointsReducer
-    }
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const rootReducer = persistCombineReducers(persistConfig,
+{
+    mapPoints : mapPointsReducer
 });
+
+const reduxPersistActions = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [...reduxPersistActions],
+          },
+    })});
+
+export const persistor = persistStore(store); 
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
